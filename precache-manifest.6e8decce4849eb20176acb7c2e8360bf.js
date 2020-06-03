1fe4c8f51e2df3bee4ca34b8dfe9b00419fafd86
unknown <barracarol@gmail.com>
2020-06-04 00:11:02 +0100

    Updates

---
+++ precache-manifest.6e8decce4849eb20176acb7c2e8360bf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feee80df0bff257fb3fc72381fe56029",
+    "url": "/mortimer-amsterdam/index.html"
+  },
+  {
+    "revision": "1bc044af7836802a90fd",
+    "url": "/mortimer-amsterdam/static/css/main.dc4864e2.chunk.css"
+  },
+  {
+    "revision": "a42b22ca34740e2bd80d",
+    "url": "/mortimer-amsterdam/static/js/2.60e5f05f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mortimer-amsterdam/static/js/2.60e5f05f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc044af7836802a90fd",
+    "url": "/mortimer-amsterdam/static/js/main.66bc147e.chunk.js"
+  },
+  {
+    "revision": "db85a376e17d669d48ff",
+    "url": "/mortimer-amsterdam/static/js/runtime-main.cedb89b7.js"
+  },
+  {
+    "revision": "21bb70b62317f276f2e97a919ff5bd8c",
+    "url": "/mortimer-amsterdam/static/media/BebasNeue-Regular.21bb70b6.ttf"
+  },
+  {
+    "revision": "2087817d114af15fb59b24970fcadaf1",
+    "url": "/mortimer-amsterdam/static/media/background-gallery.2087817d.jpg"
+  },
+  {
+    "revision": "62782278ee21591900b34e9a5cbfa3ce",
+    "url": "/mortimer-amsterdam/static/media/facade2.62782278.jpeg"
+  },
+  {
+    "revision": "7ca79faeb882841e3659a8d163b6fb18",
+    "url": "/mortimer-amsterdam/static/media/googlemaps.7ca79fae.png"
+  },
+  {
+    "revision": "ef9822dc0adb446422c69da83390b349",
+    "url": "/mortimer-amsterdam/static/media/menu.ef9822dc.jpeg"
+  },
+  {
+    "revision": "c05728d050236aac61c37c65dfb94838",
+    "url": "/mortimer-amsterdam/static/media/mortimer-background.c05728d0.jpg"
+  },
+  {
+    "revision": "02328d22befeec5a3b33277df8663b93",
+    "url": "/mortimer-amsterdam/static/media/mortimer-svg.02328d22.svg"
+  },
+  {
+    "revision": "6d98f733c5c4d14eae1a2448604e58e1",
+    "url": "/mortimer-amsterdam/static/media/rob-about.6d98f733.jpeg"
+  },
+  {
+    "revision": "7702a329728744802bba115dbd7e7e62",
+    "url": "/mortimer-amsterdam/static/media/staff.7702a329.jpeg"
+  },
+  {
+    "revision": "32c79de68440e21874ab60ea999ef1cc",
+    "url": "/mortimer-amsterdam/static/media/staff2.32c79de6.jpeg"
+  }
+]);
